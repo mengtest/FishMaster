4ba8f642810fa838868e9e61c916e375498ef5a5
xxslit <xxslit@Gmail.com>
2018-01-19 12:47:23 +0800

    水波纹效果脚本

---
+++ Assets/Scripts/Effect/EF_WaterWave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF_WaterWave : MonoBehaviour {
+
+    public Texture[] textures;
+    private Material material;
+    private int index;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        InvokeRepeating("ChangeTexture",0,0.1f);
+    }
+    private void ChangeTexture()
+    {
+        material.mainTexture = textures[index];
+        index = (index + 1) % textures.Length;
+    }
+
+}
