e728d3d8188be92c4d8f94c46f7333fd5b8ae5fb
xxslit <xxslit@Gmail.com>
2018-01-20 14:22:44 +0800

    自动移动

---
+++ Assets/Scripts/Effect/EF_AutoMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF_AutoMove : MonoBehaviour {
+
+    public float speed = 1f;
+    public Vector3 dir = Vector3.right;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(dir * speed * Time.deltaTime);
+	}
+}
