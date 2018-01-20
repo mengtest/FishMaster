e772c9988a07ff59c64e1cdae641f05293a4c9f5
xxslit <xxslit@Gmail.com>
2018-01-20 13:02:58 +0800

    孵化器位置.鱼类prefab赋值

--- Assets/Scripts/FishMaker.cs
+++ Assets/Scripts/FishMaker.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class FishMaker : MonoBehaviour {
 
-	
+    private Transform fishHolder;
+    private List<Transform> genPositions;
+    [SerializeField]
+    private GameObject[] fishPrefabs;
+
+    private void Awake()
+    {
+        fishHolder = GameObject.Find("fishHolder").transform;
+        genPositions = new List<Transform>();
+        foreach (Transform item in GameObject.Find("GenPos").transform)
+        {
+            genPositions.Add(item);
+        }
+    }
 }
