c75715cc114c80a8324fd243dad0a550db1f3669
xxslit <xxslit@Gmail.com>
2018-01-21 00:55:42 +0800

    增加死亡功能

--- Assets/Scripts/BulletAttr.cs
+++ Assets/Scripts/BulletAttr.cs
@@ -6,11 +6,20 @@ public class BulletAttr : MonoBehaviour {
 
     public int speed;
     public float damage;
-
+    public GameObject webPrefab;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Border")
         {
+            Destroy(gameObject);
+        }
+
+        if (collision.tag == "Fish")
+        {
+            GameObject web = Instantiate(webPrefab);
+            web.transform.SetParent(GameObject.Find("WebHolder").transform, false);
+            web.transform.position = transform.position;
+            web.GetComponent<WebAttr>().damage = damage;
             Destroy(gameObject);
         }
     }
