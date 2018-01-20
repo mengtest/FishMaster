7843a63a9b8bf79302c9d42e3fae35143fe8234d
xxslit <xxslit@Gmail.com>
2018-01-20 14:36:31 +0800

    自动旋转的脚本

---
+++ Assets/Scripts/Effect/EF_AutoRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF_AutoRotate : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
