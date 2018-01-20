2ae78e64f0eb455d0c2dad972a26d3aa0fef1f0f
xxslit <xxslit@Gmail.com>
2018-01-21 00:56:30 +0800

    渔网的属性

---
+++ Assets/Scripts/WebAttr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebAttr : MonoBehaviour
+{
+
+    public float disapperTime;//消失时间
+    public float damage;//攻击
+    private void Start()
+    {
+        Destroy(gameObject, disapperTime);//销毁自身
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Fish")
+        {
+            //collision.gameObject.GetComponent<FishAttr>().TakeDamage(damage);
+            collision.SendMessage("TakeDamage", damage);//给鱼发消息把,伤害传递过去
+        }
+    }
+}
