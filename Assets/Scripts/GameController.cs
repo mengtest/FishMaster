f49a364f99a785dc7049327a39a19dcec0306a00
xxslit <xxslit@Gmail.com>
2018-01-20 21:44:48 +0800

    增加按键点击与鼠标滚轮增加或减少每发炮弹消耗的功能

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] gunGos;
+    private int costIndex = 0;
+    private Text oneShootCostText;
+    private int[] oneShootCosts = { 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+	void Start () {
+        oneShootCostText = GameObject.Find("oneShootCostText").GetComponent<Text>();
+
+    }
+	
+	void Update () {
+        ChangeBulletCost();
+	}
+
+    void ChangeBulletCost()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel")<0)
+        {
+            OnButtonMDown();
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            OnButtonPDown();
+        }
+    }
+    public void OnButtonPDown()
+    {
+        gunGos[costIndex / 4].SetActive(false);
+        costIndex++;
+        costIndex = (costIndex>oneShootCosts.Length-1)?0: costIndex;
+        gunGos[costIndex / 4].SetActive(true);
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
+    }
+    public void OnButtonMDown()
+    {
+        gunGos[costIndex / 4].SetActive(false);
+        costIndex--;
+        costIndex = (costIndex < 0) ? oneShootCosts.Length - 1 : costIndex;
+        gunGos[costIndex / 4].SetActive(true);
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
+
+    }
+}
