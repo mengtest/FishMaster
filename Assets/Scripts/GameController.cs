67f175bfd78fc8e8fbeb7ed842e5f28a6d5a1c76
xxslit <xxslit@Gmail.com>
2018-01-21 01:41:25 +0800

    更新UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,8 +8,7 @@ public class GameController : MonoBehaviour {
 
     public GameObject[] gunGos;
     private int costIndex = 0;
-    [SerializeField]
-    private int lv;
+    
     private Text oneShootCostText;
     private Transform bulletHolder;
     private Transform FirePos;
@@ -22,10 +21,45 @@ public class GameController : MonoBehaviour {
     [SerializeField]
     private GameObject[] bullet4Gos;
 
+    private Text goldText;
+    private Text lvText;
+    private Text lvNameText;
+    private Text smallCountdownText;
+    private Text bigCountdownText;
+    private Button bigCountdownButton;
+    private Button backButton;
+    private Button settingButton;
+    private Slider expSlider;
+
+    [SerializeField]
+    private int lv = 0;
+    private int exp = 0;
+    private int gold = 500;
+    private const int bigCountdown = 240;
+    private const int smallCountdown = 60;
+    private float bigTimer = bigCountdown;
+    private float smallTimer = smallCountdown;
+
     private int[] oneShootCosts = { 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+    private string[] lvNames = { "新手", "入门", "青铜", "白银", "铂金", "白金", "黄金", "钻石", "大师", "宗师"};
+
+    private void Awake()
+    {
+        goldText = GameObject.Find("GoldNumText").GetComponent<Text>();
+        lvText = GameObject.Find("LvText").GetComponent<Text>();
+        lvNameText = GameObject.Find("LvNameText").GetComponent<Text>();
+        smallCountdownText = GameObject.Find("SmallCountdown").GetComponent<Text>();
+        bigCountdownText = GameObject.Find("BigCountdown").GetComponent<Text>();
+        bigCountdownButton = GameObject.Find("RewarButton").GetComponent<Button>();
+        backButton = GameObject.Find("BackButton").GetComponent<Button>();
+        settingButton = GameObject.Find("SettingButton").GetComponent<Button>();
+        expSlider = GameObject.Find("LvNameText").GetComponent<Slider>();
+    }
+
 	void Start () {
         oneShootCostText = GameObject.Find("oneShootCostText").GetComponent<Text>();
         bulletHolder = GameObject.Find("bulletHolder").transform;
+        UpdateUI();
     }
 	
     void Fire()
@@ -48,7 +82,7 @@ public class GameController : MonoBehaviour {
             bullet.transform.position = gunGos[costIndex / 4].transform.Find("FirePos").transform.position;
             bullet.transform.rotation = gunGos[costIndex / 4].transform.rotation;
             bullet.AddComponent<EF_AutoMove>().dir = Vector3.up;
-            bullet.GetComponent<EF_AutoMove>().speed =10f;
+            bullet.GetComponent<EF_AutoMove>().speed = bullet.GetComponent<BulletAttr>().speed;
             bullet.GetComponent<BulletAttr>().damage *= oneShootCosts[costIndex];
         }
     }
@@ -84,5 +118,13 @@ public class GameController : MonoBehaviour {
         gunGos[costIndex / 4].SetActive(true);
         oneShootCostText.text = "$" + oneShootCosts[costIndex];
 
+    }
+    void UpdateUI()
+    {
+        goldText.text = gold.ToString();
+        lvText.text = lv.ToString(); 
+        lvNameText.text = lvNames[lv/10].ToString();
+        smallCountdownText.text = smallCountdown.ToString();
+        bigCountdownText.text = bigCountdown.ToString() ;
     }
 }
