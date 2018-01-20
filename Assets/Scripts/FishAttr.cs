754916c17f01605a03c374e3d5fb695457dbcea9
xxslit <xxslit@Gmail.com>
2018-01-20 14:55:47 +0800

    增加碰撞到边界自动销毁的功能

--- Assets/Scripts/FishAttr.cs
+++ Assets/Scripts/FishAttr.cs
@@ -7,4 +7,12 @@ public class FishAttr : MonoBehaviour {
     public int maxNum;
     public int maxSpeed;
     public float fishGenWaitTiem = 0.5f;//每条鱼生产的间隔
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Border")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
