c80a0f9a942abe03f0353817b3dff7bc63659b73
xxslit <xxslit@Gmail.com>
2018-01-21 13:25:44 +0800

    金币移动脚本

---
+++ Assets/Scripts/Effect/EF_MoveTo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF_MoveTo : MonoBehaviour {
+
+    private GameObject goldCollect;
+    public float moveSpeed;
+	void Start () {
+        goldCollect = GameObject.Find("GoldCollect");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, goldCollect.transform.position, moveSpeed * Time.deltaTime);
+            
+	}
+}
