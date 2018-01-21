71bd459eb96f4534c8fbc3445e271f262ef25055
xxslit <xxslit@Gmail.com>
2018-01-21 13:26:23 +0800

    金币碰撞器

---
+++ Assets/Scripts/GoldCollect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCollect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Gold")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
