222c1c245cd0a5cf8e3b1713440e59352ae69c90
xxslit <xxslit@Gmail.com>
2018-01-21 14:15:01 +0800

    新游戏,加载游戏功能脚本

---
+++ Assets/Scripts/GameMananer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMananer : MonoBehaviour {
+
+    private Button newGame;
+    private Button loadGame;
+    private void Awake()
+    {
+        newGame = GameObject.Find("NewGame").GetComponent<Button>();
+        loadGame = GameObject.Find("LoadGame").GetComponent<Button>();
+        newGame.onClick.AddListener(NewGame);
+        loadGame.onClick.AddListener(LoadGame);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    private void NewGame()
+    {
+        PlayerPrefs.SetInt("Lv", 0);
+        PlayerPrefs.SetInt("Exp", 0);
+        PlayerPrefs.SetInt("Gold", 10000);
+        SceneManager.LoadScene("Main");
+    }
+}
